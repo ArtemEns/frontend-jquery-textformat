5d7ebd359a9efaa8a00adf9a630e7f469fcccf44
Artem <Artemalex1990@gmail.com>
2016-12-15 17:44:26 +0200

    все в нижний регистр, убрал проблеы вначале/вконце, множественные пробелы замененены на 1, пререносы строк в пробелы

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+
+
 'use strict';
 
-console.log($)
+var message=document.getElementByName('message');
+
+function format()
+{
+    $.message.toLowerCase;      //нижний регистр
+    $.trim(message);               // пробелы вначале и конце
+    $.message.replace(/\s+/g," ");  //все множественные пробелы замена на 1 пробел
+    $.message.replace(/\n+/g," ");   //перенос строки > в пробел
+    
+}
+
+var input = $.message-field;
+$.input.output = function
+ {document.getElementByName('message').innerHTML=input.value};    //взял с интернета, "как сделать так чтоб текст сразу выводился"
+  
+
+$.message(format);                   //применить функцию format  к полю в которое вводим текст
+$.output(input.message);                 // вывел в output поле отформатированный текст
+
+console.log($.output)
+
+
+
+
+
+//   var spacing = new spacing(/^\s+/g);  //какаято хрень из интернета которую я не понимаю
