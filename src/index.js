fefc2a0eb72130de485ead44a07f397a5c5e973b
Artem <Artemalex1990@gmail.com>
2016-12-20 22:12:46 +0200

    работает

--- src/index.js
+++ src/index.js
@@ -1,30 +1,16 @@
-
-
 'use strict';
 
-var message=document.getElementByName('message');
+var message = $('.message-field');
+var formatted = $('.message-formatted');
 
-function format()
-{
-    $.message.toLowerCase;      //нижний регистр
-    $.trim(message);               // пробелы вначале и конце
-    $.message.replace(/\s+/g," ");  //все множественные пробелы замена на 1 пробел
-    $.message.replace(/\n+/g," ");   //перенос строки > в пробел
-    
+function reFormed()
+ {
+    var reformedText = message.val()
+        .toLowerCase()
+        .trim()
+        .replace(/\s+/g, " ")
+        .replace(/\n+/g, " ");
+    formatted.text(reformedText);
 }
 
-var input = $.message-field;
-$.input.output = function
- {document.getElementByName('message').innerHTML=input.value};    //взял с интернета, "как сделать так чтоб текст сразу выводился"
-  
-
-$.message(format);                   //применить функцию format  к полю в которое вводим текст
-$.output(input.message);                 // вывел в output поле отформатированный текст
-
-console.log($.output)
-
-
-
-
-
-//   var spacing = new spacing(/^\s+/g);  //какаято хрень из интернета которую я не понимаю
+message.on('input', reFormed);
